2b905efdb4a570151fc0ec26a4325eb33bef7429
AmitShib <amitshiber2001@gmail.com>
2023-07-17 12:44:41 +0300

    part1 of task1

--- MongoDB/task1.js
+++ MongoDB/task1.js
@@ -1,6 +1,7 @@
 const { MongoClient } = require("mongodb");
 
-const uri = "mongodb://localhost:27017";
+// const uri = "mongodb://localhost:27017/?readPreference=primary&ssl=false&directConnection=true";
+const uri = "mongodb://127.0.0.1:27017/";
 
 // Function to connect to the MongoDB database
 async function connectToDatabase() {
@@ -17,19 +18,29 @@ async function connectToDatabase() {
         const collection = database.collection("vehichles");
 
         //1
-
+        console.log("MISSION 1:");
         console.log(await collection.findOne({ id: "55373221" }));
         //2
+        console.log("MISSION 2:");
         console.log(await collection.find({ status: "mission" }).project({ _id: 0, id: 1 }).toArray());
         //3
+        console.log("MISSION 3:");
         console.log(await collection.find({ status: { $ne: "mission" } }).project({ _id: 0, id: 1 }).toArray());
         //4
-        console.log(await collection.find({}).sort({ date: 1 }).limit(3));
+        console.log("MISSION 4:");
+        console.log(await collection.find({}).sort({ date: 1 }).limit(3).toArray());
         //5
+        console.log("MISSION 5:");
         console.log(await collection.aggregate([{ $sort: { date: -1 } }, { $limit: 3 }]).toArray());
         //6
+        console.log("MISSION 6:");
+        console.log(await collection.find({manufacturinfD:{$gt: new Date(2010,1,1)}}).toArray());
         //7
+        console.log("MISSION 7:");
+        console.log(await collection.find({"myArr": {"$exists": true}}).toArray());
         //8
+        console.log("MISSION 8:");
+        console.log(await collection.find({"myArr.2":{$exists:1}}).toArray());
 
     } catch (error) {
         console.error("Error:", error);
