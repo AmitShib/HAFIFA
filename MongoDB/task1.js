bb49a7041b4ab83b651eff3d4e19bb1e1e632c25
AmitShib <amitshiber2001@gmail.com>
2023-07-17 10:30:17 +0300

    task1Mongo

--- MongoDB/task1.js
+++ MongoDB/task1.js
@@ -1,11 +1,3 @@
-// import {MongoClient} from 'mongodb';
-
-// MongoClient.connect('mongodb://localhost:27017/?readPreference=primary&ssl=false&directConnection=true',
-// async (err, client)=>{
-//     if(err) throw err;
-//     console.log('connected to mongoDB succsessfully');
-// })
-
 const { MongoClient } = require("mongodb");
 
 const uri = "mongodb://localhost:27017";
@@ -13,28 +5,39 @@ const uri = "mongodb://localhost:27017";
 // Function to connect to the MongoDB database
 async function connectToDatabase() {
     console.log("here");
-  const client = new MongoClient(uri);
-  
-  try {
-    // Connect to the MongoDB database
-    await client.connect();
-    console.log("Connected to the database");
+    const client = new MongoClient(uri);
 
-    // Use the desired database and collection
-    const database = client.db("test");
-    const collection = database.collection("vehichles");
+    try {
+        // Connect to the MongoDB database
+        await client.connect();
+        console.log("Connected to the database");
 
-    // Perform your queries here
-    // Example: Find a document with a specific value
-    const result = await collection.findOne({id: "55373221"});
-    console.log("Found document:", result);
-  } catch (error) {
-    console.error("Error:", error);
-  } finally {
-    // Close the connection when done
-    await client.close();
-    console.log("Connection closed");
-  }
+        // Use the desired database and collection
+        const database = client.db("test");
+        const collection = database.collection("vehichles");
+
+        //1
+
+        console.log(await collection.findOne({ id: "55373221" }));
+        //2
+        console.log(await collection.find({ status: "mission" }).project({ _id: 0, id: 1 }).toArray());
+        //3
+        console.log(await collection.find({ status: { $ne: "mission" } }).project({ _id: 0, id: 1 }).toArray());
+        //4
+        console.log(await collection.find({}).sort({ date: 1 }).limit(3));
+        //5
+        console.log(await collection.aggregate([{ $sort: { date: -1 } }, { $limit: 3 }]).toArray());
+        //6
+        //7
+        //8
+
+    } catch (error) {
+        console.error("Error:", error);
+    } finally {
+        // Close the connection when done
+        await client.close();
+        console.log("Connection closed");
+    }
 }
 
 // Call the function to start the database connection and perform queries
