5d8727a0fbddff1cf98b215feb7964c9446358ec
AmitShib <amitshiber2001@gmail.com>
2023-08-30 14:34:13 +0300

    end of robomongo task

--- MongoDB/roboVec.js
+++ MongoDB/roboVec.js
@@ -9,3 +9,72 @@ db.getCollection("vehichles").aggregate(
   );
 
   //2
+  db.getCollection("vehichles").aggregate([
+    {
+      $unwind: "$myArr"
+    },
+    {
+      $match: {
+        "myArr.kind": "oil"
+      }
+    },
+    {
+      $group: {
+        _id: null,
+        oilObjects: {
+          $push: "$myArr"
+        }
+      }
+    }
+  ])
+
+  //3
+  db.getCollection("vehichles").aggregate([
+    {
+      $unwind: "$type"
+    },
+    {
+      $group: {
+        _id: "$type.kind",
+        oldestVehicle: {
+          $min: "$_id"
+        },
+        newestVehicle: {
+          $max: "$_id"
+        }
+      }
+    },
+    {
+      $project: {
+        kind: "$_id",
+        oldestVehicle: 1,
+        newestVehicle: 1,
+        _id: 0
+      }
+    }
+  ])
+
+  //4
+  db.getCollection("vehichles").aggregate([
+    {
+      $addFields: {
+        manufacturingYear: { $year: { $toDate: "$type.manufacturinfD" } },
+        manufacturingMonth: { $month: { $toDate: "$type.manufacturinfD" } }
+      }
+    },
+    {
+      $match: {
+        $or: [
+          { manufacturingMonth: 7 },
+          { manufacturingMonth: 8 }
+        ]
+      }
+    },
+    {
+      $project: {
+        _id: 0,
+        id: 1,
+        manufacturingYear: 1
+      }
+    }
+  ])
