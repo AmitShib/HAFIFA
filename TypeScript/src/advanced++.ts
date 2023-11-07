0c6d6ce88c0a2edbf627f160862ca0964ce5ed5f
AmitShib <amitshiber2001@gmail.com>
2023-11-07 19:29:35 +0200

    the start of the last chapter

---
+++ TypeScript/src/advanced++.ts
@@ -0,0 +1,10 @@
+type PersonalNumber  = `${string}@idf.ts`;
+
+const validString1: PersonalNumber  = 'example@idf.ts';
+
+console.log(validString1);
+
+type DayOfWeek = 'sunday' | 'monday' | 'tuesday';
+  
+  
+    
