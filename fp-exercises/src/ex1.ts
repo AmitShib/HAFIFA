1d67361d9453d215c991b507cd23a370e726b2d2
AmitShib <amitshiber2001@gmail.com>
2023-11-28 20:18:35 +0200

    ex1 FP complete

--- fp-exercises/src/ex1.ts
+++ fp-exercises/src/ex1.ts
@@ -1,28 +1,43 @@
-import { range, filter } from 'ramda';
+import { range, filter, lte, __, lt, map, multiply, when, sort, uniq, nth, pipe, isNil, ifElse, modulo, equals, curry, length, gt } from 'ramda';
 
 // write a function that gets an array of scores and removes any scores that are higher than 100.
-export const removeOverScores = (arr: number[]): number[] => arr.filter(elem => elem <= 100);
-
-
+export const removeOverScores = filter(lte(__, 100));
 
 // write a function that gets an array of scores and multiplies by 10 all the scores that are below 10.
-const isBelowTen =(num :number) => num < 10;
-const multByTen = (num:number):number=> num*10;
+const isBelowTen = lt(__, 10);
+const multByTen = multiply(10);
 
-export const boostSingleScores = (arr: number[]):number[]=>arr.map((num: number) => (isBelowTen(num) ? multByTen(num) : num));
-
+export const boostSingleScores = map(when(isBelowTen, multByTen));
 
 // write a function that gets an array of scores returns the second largest value.
 // const maxElem = (arr:number[]):number => Math.max(...arr);
 // export const secondMaximum =(arr:number[]):number=>Math.max(...arr.filter(elem=>(elem<maxElem(arr))));
-const sortArr = (arr:number[]):number[] => arr.sort((a, b) => b - a);
-export const secondMaximum =(arr:number[]):number=>sortArr(arr)[1];
-
+const sortNumArr = sort<number>((a, b) => b - a);
+// const sortArr = sort(flip(subtract));
+export const secondMaximum = pipe(
+    sortNumArr,
+    uniq,
+    ifElse(pipe(
+        nth(1), isNil
+    ),
+        nth(0), nth(1))
+);
+//export const secondMaximum = (arr: number[]): number => sortNumArr(arr)[1];
 
 // write a function that gets a number and returns all number's dividers
-const isDivider = (num:number, div: number) => num % div === 0;
-export const getDividers =(num:number):number[]=> filter((i: number) => isDivider(num, i), range(1, num + 1));
+const isDivider = curry(pipe(
+    modulo,
+    equals(0)
+));
+
+
+export const getDividers = (num: number): number[] => range(1, num + 1).filter(isDivider(num));
 
 
 // write a function that gets a number and returns true if the number is prime, or else returns false
-export const isPrime =(num:number):boolean=> (getDividers(num).length > 2) ;
+// export const isPrime = (num: number): boolean => (getDividers(num).length > 2);
+export const isPrime = pipe(
+    getDividers,
+    length,
+    gt(__, 2)
+);
