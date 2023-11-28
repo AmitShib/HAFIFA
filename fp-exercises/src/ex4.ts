810b7f996525472552b797f76ae26d96d4738dff
AmitShib <amitshiber2001@gmail.com>
2023-11-28 15:47:25 +0200

    final cosmetic to ex4 FP

--- fp-exercises/src/ex4.ts
+++ fp-exercises/src/ex4.ts
@@ -1,25 +1,14 @@
 interface MapFunction<T, V> {
-    (value: T): V;
+  (value: T): V;
 }
 // implement map function using reduce
-export const map = <T, V>(arr: T[], mapFunc: MapFunction<T, V>): V[] =>{
-    return arr.reduce((result, currentValue) => {
-        const mappedValue = mapFunc(currentValue);
-        result.push(mappedValue);
-        return result;
-      }, [] as V[]);
-};
+export const map = <T, V>(arr: T[], mapFunc: MapFunction<T, V>): V[] =>
+arr.reduce((result, value) => [...result, mapFunc(value)], [] as V[]);
 
 
 interface Predicate<T> {
-    (value: T): boolean;
+  (value: T): boolean;
 }
 // implement filter function using reduce
-export const filter = <T>(arr: T[], predicate: Predicate<T>): T[] =>{
-    return arr.reduce((result, currentValue) => {
-        if (predicate(currentValue)) {
-          result.push(currentValue);
-        }
-        return result;
-      }, [] as T[]);
-};
+export const filter = <T>(arr: T[], predicate: Predicate<T>): T[] =>
+arr.reduce((result, value) => (predicate(value) ? [...result, value] : result), [] as T[]);
